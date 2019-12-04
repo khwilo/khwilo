9b3d1053a420c63317f0c3768c46098fb4fd050b
Khwilo Kabaka <khwilowatai@gmail.com>
2019-12-04 15:42:06 +0300

    Add FontAwesome icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavigationComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, NavigationComponent],
+  imports: [BrowserModule, AppRoutingModule, FontAwesomeModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import {
+  faGithub,
+  faLinkedin,
+  faTwitter
+} from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-navigation',
@@ -7,6 +12,13 @@ import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 })
 export class NavigationComponent implements OnInit {
   emailAddress = 'khwilowatai@gmail.com';
+  GitHubLink = 'https://github.com/khwilo';
+  LinkedInLink = 'www.linkedin.com/in/khwilo-watai';
+  TwitterLink = 'https://twitter.com/khwilo';
+
+  GitHubIcon = faGithub;
+  LinkedinIcon = faLinkedin;
+  TwitterIcon = faTwitter;
 
   @ViewChild('modal', { static: false })
   modal: ElementRef;
