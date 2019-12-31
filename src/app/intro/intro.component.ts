674ac441a9bf2d29ef405c1c8817ea54390aa523
Khwilo Kabaka <khwilowatai@gmail.com>
2019-12-31 16:09:59 +0300

    Add programming languages icons

--- src/app/intro/intro.component.ts
+++ src/app/intro/intro.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
+import {
+  faAngular,
+  faCss3,
+  faHtml5,
+  faJs,
+  faNodeJs
+} from '@fortawesome/free-brands-svg-icons';
+
 @Component({
   selector: 'app-intro',
   templateUrl: './intro.component.html',
   styleUrls: ['./intro.component.css']
 })
 export class IntroComponent implements OnInit {
+  HTMLIcon = faHtml5;
+  CSS3Icon = faCss3;
+  JSIcon = faJs;
+  Angular = faAngular;
+  NodeJS = faNodeJs;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
