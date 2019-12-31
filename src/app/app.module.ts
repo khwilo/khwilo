2bcd553859129cb2708d5593c96c580649d6cdde
Khwilo Kabaka <khwilowatai@gmail.com>
2019-12-31 15:29:55 +0300

    Add about section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,10 @@ import { NavigationComponent } from './navigation/navigation.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { IntroComponent } from './intro/intro.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
-  declarations: [AppComponent, NavigationComponent, ProjectsComponent, NotFoundComponent, IntroComponent],
+  declarations: [AppComponent, NavigationComponent, ProjectsComponent, NotFoundComponent, IntroComponent, AboutComponent],
   imports: [BrowserModule, AppRoutingModule, FontAwesomeModule],
   providers: [],
   bootstrap: [AppComponent]
