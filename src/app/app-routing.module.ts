8697abfe7bc001b803953a03d03d33f169ae0612
Khwilo Kabaka <khwilowatai@gmail.com>
2019-12-31 13:44:00 +0300

    Add projects section

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { IntroComponent } from './intro/intro.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 const routes: Routes = [
   { path: 'home', component: IntroComponent },
+  { path: 'projects', component: ProjectsComponent },
   { path: '', pathMatch: 'full', redirectTo: 'home' },
   { path: '**', component: NotFoundComponent }
 ];
