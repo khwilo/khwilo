d809651995447eeb270b62cbb5fe0ef3eadf9386
Khwilo Kabaka <khwilowatai@gmail.com>
2020-01-01 15:07:25 +0300

    Add contact info to about page

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
+import {
+  faGithub,
+  faLinkedin,
+  faTwitter,
+  faWhatsapp
+} from '@fortawesome/free-brands-svg-icons';
+
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
+  emailAddress: string = 'khwilowatai@gmail.com';
+  GitHubLink = 'https://github.com/khwilo';
+  LinkedInLink = 'https://www.linkedin.com/in/khwilo-watai';
+  TwitterLink = 'https://twitter.com/khwilo';
+  WhatsappLink = 'https://api.whatsapp.com/send?phone=+254718802061';
 
-  constructor() { }
+  GitHubIcon = faGithub;
+  LinkedinIcon = faLinkedin;
+  TwitterIcon = faTwitter;
+  WhatsappIcon = faWhatsapp;
 
-  ngOnInit() {
-  }
+  constructor() {}
 
+  ngOnInit() {}
 }
