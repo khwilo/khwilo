e56b9068b8738d78be12488513e0cf1dfb496a09
Khwilo Kabaka <khwilowatai@gmail.com>
2020-01-01 15:04:58 +0300

    Remove the modal display

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,10 +1,4 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import {
-  faGithub,
-  faLinkedin,
-  faTwitter,
-  faWhatsapp
-} from '@fortawesome/free-brands-svg-icons';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navigation',
@@ -12,29 +6,7 @@ import {
   styleUrls: ['./navigation.component.css']
 })
 export class NavigationComponent implements OnInit {
-  emailAddress = 'khwilowatai@gmail.com';
-  GitHubLink = 'https://github.com/khwilo';
-  LinkedInLink = 'https://www.linkedin.com/in/khwilo-watai';
-  TwitterLink = 'https://twitter.com/khwilo';
-  WhatsappLink = 'https://api.whatsapp.com/send?phone=+254718802061';
-
-  GitHubIcon = faGithub;
-  LinkedinIcon = faLinkedin;
-  TwitterIcon = faTwitter;
-  WhatsappIcon = faWhatsapp;
-
-  @ViewChild('modal', { static: false })
-  modal: ElementRef;
-
   constructor() {}
 
   ngOnInit() {}
-
-  displayModal() {
-    this.modal.nativeElement.style.display = 'block';
-  }
-
-  closeModal() {
-    this.modal.nativeElement.style.display = 'none';
-  }
 }
