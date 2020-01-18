d4e4cc80be101085db1543e40a00cac1afe06fe0
Khwilo Kabaka <khwilowatai@gmail.com>
2020-01-18 13:21:16 +0300

    Remove routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NavigationComponent } from './navigation/navigation.component';
-import { ProjectsComponent } from './projects/projects.component';
-import { NotFoundComponent } from './not-found/not-found.component';
-import { IntroComponent } from './intro/intro.component';
 import { AboutComponent } from './about/about.component';
+import { AppComponent } from './app.component';
+import { IntroComponent } from './intro/intro.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 @NgModule({
-  declarations: [AppComponent, NavigationComponent, ProjectsComponent, NotFoundComponent, IntroComponent, AboutComponent],
-  imports: [BrowserModule, AppRoutingModule, FontAwesomeModule],
+  declarations: [
+    AppComponent,
+    NavigationComponent,
+    ProjectsComponent,
+    NotFoundComponent,
+    IntroComponent,
+    AboutComponent
+  ],
+  imports: [BrowserModule, FontAwesomeModule],
   providers: [],
   bootstrap: [AppComponent]
 })
