0b5a64e36f628baefcbcbd56f7d3b40e2f229a3b
Khwilo Kabaka <khwilowatai@gmail.com>
2020-01-30 17:31:11 +0300

    Add a navigation drawer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  AfterViewInit,
+  ViewChildren,
+  QueryList,
+  ElementRef
+} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'khwilo';
+
+  @ViewChildren('list_item') listItems: QueryList<ElementRef>;
+
+  openMenu() {
+    document.body.classList.toggle('nav-open');
+  }
+
+  ngAfterViewInit() {
+    this.listItems.forEach((list) => {
+      (list.nativeElement as HTMLElement).addEventListener('click', () => {
+        document.body.classList.remove('nav-open');
+      });
+    });
+  }
 }
